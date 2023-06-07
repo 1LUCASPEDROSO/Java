a1b9dc8d8ab7d294469e61a92516e8b5835aebfa
lucas pedroso <pedrosolucas1745@gmail.com>
2023-06-07 12:03:52 -0300

    subindo versão 1.1 usando intellij

--- javaInterfaceBD/AppPapelaria/src/apppapelaria/AppPapelaria.java
+++ javaInterfaceBD/AppPapelaria/src/apppapelaria/AppPapelaria.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package apppapelaria;
+import java.util.*;
 /**
  *
  * @author 182120037
@@ -13,8 +9,12 @@ public class AppPapelaria {
     /**
      * @param args the command line arguments
      */
+     String resposta;
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite aqui");
+        String resposta = read.next();
+        System.out.println("sua resposta foi:"+resposta);
     }
 
 }
