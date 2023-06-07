100883ccd3fdb3830da9ce15deb2daeecff4d9d2
lucas pedroso <pedrosolucas1745@gmail.com>
2023-06-07 10:52:54 -0300

    subindo versão 1

---
+++ javaInterfaceBD/AppPapelaria/src/apppapelaria/AppPapelaria.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apppapelaria;
+/**
+ *
+ * @author 182120037
+ */
+public class AppPapelaria {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
