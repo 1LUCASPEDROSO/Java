109c010c00f8af4ecd36e98ab170821a34edbb60
lucas pedroso <pedrosolucas1745@gmail.com>
2023-06-20 12:13:11 -0300

    subindo versão 1.0 do código morse

---
+++ javaInterfaceBD/testesDeCodigo/src/appCodeMorse/gestData.java
@@ -0,0 +1,18 @@
+package appCodeMorse;
+import java.util.*;
+
+public class gestData {
+    datatypes DT = new datatypes();
+    Scanner read = new Scanner(System.in);
+    String  write;
+    public void translateToMorse()
+    {
+        System.out.print("Digit here your text to convert Morse: ");
+        write = read.nextLine().toLowerCase();
+        for (int i=0;i<write.length();i++) {
+            char j = write.charAt(i);
+            j = CharTo
+            System.out.println(j);
+            }// first for
+        }// method
+}// class
---
+++ javaInterfaceBD/testesDeCodigo/src/appCodeMorse/main.java
@@ -0,0 +1,8 @@
+package appCodeMorse;
+
+public class main {
+    public static void main(String[] args) {
+        gestData GD = new gestData();
+        GD.translateToMorse();
+    }// main
+}// class
