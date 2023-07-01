ccd371188aff9336994f38c85d28644093206cf7
lucas pedroso <pedrosolucas1745@gmail.com>
2023-07-01 20:28:10 -0300

    subindo versão 1 do esto que com inserirProdutos e metodos prontos

---
+++ javaInterfaceBD/testesDeCodigo/InterfaceEstoque/src/main/java/InterFace/DadosEstoque.java
@@ -0,0 +1,45 @@
+package Interface;
+
+public class DadosEstoque {
+
+
+    public String getNomeproduto() {
+        return Nomeproduto;
+    }
+
+    public void setNomeproduto(String nomeproduto) {
+        Nomeproduto = nomeproduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPrecoUnitário() {
+        return precoUnitário;
+    }
+
+    public void setPrecoUnitário(float precoUnitário) {
+        this.precoUnitário = precoUnitário;
+    }
+
+    private String Nomeproduto;
+    private String descricao;
+    private int quantidade;
+    private float precoUnitário;
+
+
+
+}
---
+++ javaInterfaceBD/testesDeCodigo/InterfaceEstoque/src/main/java/InterFace/main.java
@@ -0,0 +1,39 @@
+package Interface;
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        char volta;
+        int escolhaAcao;
+        GestEstoque GE = new GestEstoque();
+
+        do {
+            System.out.println("================================================================================");
+            System.out.println("Digite a operaçao que deseja realizar");
+            System.out.println(" 1 - Cadastrar \n 2 - Listar \n 3 - Deletar \n 4 - Atualizar \n 5 - Comprar");
+            System.out.print("Digite aqui: ");
+            escolhaAcao = read.nextInt();
+            switch (escolhaAcao)
+            {
+                case 1:
+                    GE.inserirDados();
+                    break;
+                case 2:
+                    GE.ListarTodosProdutos();
+                    break;
+                case 3:
+                    GE.DeletarProdutos();
+                    break;
+                case 4:
+                    GE.AtualizarProduto();
+                    break;
+                case 5:
+                    GE.ComprarProduto();
+                default:
+                    System.err.println();
+            }
+            System.out.print("Deseja fazer uma nova operação \n  s - SIM n - NÃO: ");
+            volta = read.next().charAt(0);
+        }while( volta =='s');
+    }
+}
