be67748476cce7960e583067855407ada66b52d9
lucas pedroso <pedrosolucas1745@gmail.com>
2023-07-23 22:51:39 -0300

    subindo versão 0.5 de sistema

---
+++ ProjectStore/db_store.sql
@@ -0,0 +1,27 @@
+create database db_store; 
+use db_store;
+create table product (
+ id_product int not null auto_increment primary key,
+ name_product varchar(100) not null,
+ quantity_product int not null,
+ price_product double not null
+);
+create table method_payment
+(
+id_method int not null auto_increment primary key,
+name_method varchar(50) not null
+);
+create table transition
+(
+id_transition int not null auto_increment primary key,
+total_transition int not null,
+payment_method int not null,
+product_id int not null,
+foreign key (payment_method) references method_payment(id_method),
+foreign key (product_id) references product(id_product)
+);
+
+insert into method_payment values (null,'debito'),
+								  (null,'crédito'),
+                                  (null,'pix'),
+                                  (null,'crypto')
